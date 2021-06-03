a3a57a0c4b6e14a46fba08e94ac5dc855b5003f2
Khalil Abuayyash <khalilayyash@gmail.com>
2021-06-03 16:50:42 +0300

    day3 partials

--- student/views.py
+++ student/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'home.html')
+
+def header(request):
+    return render(request, 'header.html')
+
+def sidebar(request):
+    return render(request, 'sidebar.html')
